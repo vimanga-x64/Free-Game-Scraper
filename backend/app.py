e0305e45bc423ab632e5dbe1e0942099c04caba4
vimanga-x64 <vishudha98@gmail.com>
2025-04-07 23:29:23 -0400

    Changed app.py to specify ports

--- backend/app.py
+++ backend/app.py
@@ -66,4 +66,5 @@ def update_free_games():
     return jsonify({"message": "Scrape complete", "data": data})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
